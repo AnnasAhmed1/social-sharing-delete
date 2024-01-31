523fd006708934e58b99f50e6fec594defe4eab5
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-01-31 22:08:11 +0500

    123

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import HelmetMetaData from "./helmet";
 
 function App() {
   return (
     <div className="App">
-      <HelmetMetaData />
+      <Helmet>
+        <title>{"pageTitle"}</title>
+        <meta name="description" content={"pageDescription"} />
+        <meta
+          property="og:image"
+          content={
+            "https://media.istockphoto.com/id/1455965102/photo/beautiful-sunrise-bursting-through-the-eucalyptus-trees-as-it-rises-over-a-mountain-beside-a.jpg?s=1024x1024&w=is&k=20&c=wYGK__qz9i8M7NfBvkNtkfbWNoiBxDLGi64PQjOo_wY="
+          }
+        />
+        {/* Add more meta tags as needed */}
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
