e4ee3fd407a2fdf52e214f57603d1eee13536856
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-01 13:59:32 +0500

    33321

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 // src/App.js
 import React, { useEffect } from "react";
-import Home from "./home";
+import Home, { Head } from "./home";
+import { Helmet } from "react-helmet";
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="og:image"
+          content={
+            "https://media.istockphoto.com/id/1455965102/photo/beautiful-sunrise-bursting-through-the-eucalyptus-trees-as-it-rises-over-a-mountain-beside-a.jpg?s=1024x1024&w=is&k=20&c=wYGK__qz9i8M7NfBvkNtkfbWNoiBxDLGi64PQjOo_wY="
+          }
+        />
+        {/* <Head></Head> */}
+      </Helmet>
+      <Home></Home>
+    </>
+  );
 };
 
 export default App;
