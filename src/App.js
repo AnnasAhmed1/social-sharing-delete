a2a8beba05a82a8c33ab124467d6884e04247c6d
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-01 12:25:42 +0500

    chking

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Page1 from "./page1";
-import Page2 from "./page2";
+// src/App.js
+import React, { useEffect } from "react";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Page1 />,
-  },
-  {
-    path: "/page2",
-    element: <Page2 />,
-  },
-]);
-function App() {
-  return <RouterProvider router={router} />;
-}
+const App = () => {
+  useEffect(() => {
+    // Update og:image meta tag dynamically
+    const ogImageTag = document.querySelector('meta[property="og:image"]');
+    console.log(ogImageTag, "image_tag");
+    if (ogImageTag) {
+      ogImageTag.content =
+        "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I=";
+    }
+    const ogDescTag = document.querySelector('meta[property="og:description"]');
+    console.log(ogDescTag, "desc_tag");
+    if (ogDescTag) {
+      ogDescTag.content = "Mazaar e quiad";
+    }
+  }, []);
+
+  return (
+    <div>
+      {/* Your component content */}
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
 
 export default App;
