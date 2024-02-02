3e687446ca48465b3385ac9759def1cdaafc5923
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-02 15:06:03 +0500

    dom-chk

--- src/page2.jsx
+++ src/page2.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import HelmetMetaData from "./helmet";
-import { Helmet } from "react-helmet";
+import React, { useEffect } from "react";
+// import { Helmet } from "react-helmet";
 
 const Page2 = () => {
+  useEffect(() => {
+    const metaTag = document.createElement("meta");
+
+    metaTag.setAttribute("property", "og:image");
+    metaTag.setAttribute(
+      "content",
+      "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+    );
+
+    document.head.appendChild(metaTag);
+  }, []);
   return (
     <div>
-      <Helmet>
+      {/* <Helmet>
         <title>My Title</title>
         <meta
           property="og:image"
           content="https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
         />
-      </Helmet>
+      </Helmet> */}
       Page2
     </div>
   );
