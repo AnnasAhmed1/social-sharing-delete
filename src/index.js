4e87290f14102a78043df01f57e7accb8ee6426d
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-02 15:19:15 +0500

    dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const metaTag = document.createElement("meta");
+
+metaTag.setAttribute("property", "og:image");
+metaTag.setAttribute(
+  "content",
+  "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+);
+document.head.appendChild(metaTag);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
--- src/page2.jsx
+++ src/page2.jsx
@@ -3,15 +3,15 @@ import React, { useEffect } from "react";
 
 const Page2 = () => {
   useEffect(() => {
-    const metaTag = document.createElement("meta");
+    // const metaTag = document.createElement("meta");
 
-    metaTag.setAttribute("property", "og:image");
-    metaTag.setAttribute(
-      "content",
-      "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
-    );
+    // metaTag.setAttribute("property", "og:image");
+    // metaTag.setAttribute(
+    //   "content",
+    //   "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+    // );
 
-    document.head.appendChild(metaTag);
+    // document.head.appendChild(metaTag);
   }, []);
   return (
     <div>
