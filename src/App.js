5844a7ddd590341b9bb50fd60aeb64f717eea59a
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-02 19:23:58 +0500

    asyn-helmet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-// src/App.js
-import React, { useEffect } from "react";
-import Home, { Head } from "./home";
-import { Helmet } from "react-helmet";
-import Page1 from "./page1";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Page2 from "./page2";
+// App.js
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Page1 />,
-    },
-    {
-      path: "/page2",
-      element: <Page2 />,
-    },
-  ]);
-  return <RouterProvider router={router} />;
-};
+const App = () => (
+  <HelmetProvider>
+    <Helmet>
+      <title>Page Title</title>
+      <meta
+        property="og:image"
+        content="https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+      />
+    </Helmet>
+    <h1>Hello World</h1>
+    {/* Your other application components go here */}
+  </HelmetProvider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,24 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const metaTag = document.createElement("meta");
+// const metaTag = document.createElement("meta");
 
-metaTag.setAttribute("property", "og:image");
-metaTag.setAttribute(
-  "content",
-  "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
-);
-// document.head.appendChild(metaTag);
+// metaTag.setAttribute("property", "og:image");
+// metaTag.setAttribute(
+//   "content",
+//   "https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+// );
+// // document.head.appendChild(metaTag);
 
-// const meta = ReactDOM.hydrateRoot(document.getElementById("head"));
-// meta.render(metaTag);
-const meta = ReactDOM.createRoot(document.getElementById("head"));
-meta.render(
-  <meta
-    property="og:image"
-    content="https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
-  />
-);
+// // const meta = ReactDOM.hydrateRoot(document.getElementById("head"));
+// // meta.render(metaTag);
+// const meta = ReactDOM.createRoot(document.getElementById("head"));
+// meta.render(
+//   <meta
+//     property="og:image"
+//     content="https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+//   />
+// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
